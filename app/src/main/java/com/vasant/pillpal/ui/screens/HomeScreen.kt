ca53acdc07bd9cb6bb106e7c0adb0a8329b51d00
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-15 22:32:43 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/vasant/pillpal/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/vasant/pillpal/ui/screens/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import com.vasant.pillpal.ui.components.BottomNavigationBar
 import com.vasant.pillpal.ui.components.HomeContent
 import com.vasant.pillpal.ui.components.TopBarHomeScreen
