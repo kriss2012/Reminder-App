e1e863a26219911f3f0ac19db2e12e15aecb3b27
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-16 09:38:57 +0530

    Update GuestLoginScreen.kt

--- app/src/main/java/com/vasant/pillpal/ui/screens/AuthScreens/GuestLoginScreen.kt
+++ app/src/main/java/com/vasant/pillpal/ui/screens/AuthScreens/GuestLoginScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
