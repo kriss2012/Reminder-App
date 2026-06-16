e04f88546778c3a256b3622cdbb54e1650bedd4e
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-16 10:42:55 +0530

    Update Auth.kt

--- app/src/main/java/com/vasant/pillpal/repository/Auth.kt
+++ app/src/main/java/com/vasant/pillpal/repository/Auth.kt
@@ -5,5 +5,6 @@ import com.vasant.pillpal.utils.AuthState
 interface Auth {
     suspend fun SingIn(email: String, password: String): AuthState
     suspend fun SingUp(email: String, password: String): AuthState
+    suspend fun updatePassword(email: String, oldPass: String, newPass: String): AuthState
     fun LogOut()
 }
